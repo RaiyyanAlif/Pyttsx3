2c14f297a6b1075bce93dce412fb81004207fac8
RaiyyanAlif <69851090+RaiyyanAlif@users.noreply.github.com>
2020-08-25 10:45:54 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+import pyttsx3
+
+friend = pyttsx3.init()
+friend.say("Hey you whats up guys its mr. triple r and welcome to my channel niye aslam tumader jonno aro ekta special video")
+friend.runAndWait()
